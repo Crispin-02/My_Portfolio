b39628b596387850021260883208e5d47d6cbc21
Crispin-02 <crispinpatric021102@gmail.com>
2024-08-21 23:11:52 +0530

    f c 4

---
+++ src/assets/index.js
@@ -0,0 +1,36 @@
+import logo from "./logo.png";
+import canva from "./canva.png";
+import unity from "./unity.png";
+import blender from "./blender.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import login from "./login.png";
+import matchbox from "./matchbox.png";
+import amongus from "./amongus.png";
+
+export {
+  logo,
+  canva,
+  unity,
+  figma,
+  blender,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  reactjs,
+  login,
+  matchbox,
+  amongus,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000",
+        secondary: "#85848a",
+        tertiary: "#545454",
+        "black-100": "#1c1a1a",
+        "black-200": "#0f0f0f",
+        "white-100": "#ffffff",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
