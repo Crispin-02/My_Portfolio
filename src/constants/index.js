ec652a9d8229b04af4b50a7793379dcc6f8a5ea4
Crispin-02 <crispinpatric021102@gmail.com>
2024-10-25 12:12:47 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -210,7 +210,7 @@ const projects = [
       },
     ],
     image: spotify,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://www.figma.com/proto/DLKg0Mm5QHzqAS9d2dyUjo/Untitled?node-id=1-2&node-type=canvas&t=GelU2kWF8gOCpmrd-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2",
   },
   {
     name: "Match Box",
