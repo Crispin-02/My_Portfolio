e915caefc2d4c1cdc530ec11183e1d87997a109f
Crispin-02 <crispinpatric021102@gmail.com>
2024-10-30 21:25:10 +0530

    fc17

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,10 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { 
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas 
+} from './canvas';
+
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -8,7 +14,6 @@ import Works from "./Works";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
-
 
 export {
   Hero,
